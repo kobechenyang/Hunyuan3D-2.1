c510ce49658050cd6aa3ecbd115b64fdde064d64
kobechenyang <kobechenyang@gmail.com>
2025-09-30 18:12:20 -0700

    fix trimesh error

--- hy3dpaint/utils/simplify_mesh_utils.py
+++ hy3dpaint/utils/simplify_mesh_utils.py
@@ -29,9 +29,9 @@ def mesh_simplify_trimesh(inputpath, outputpath, target_count=40000):
         ms.load_new_mesh(inputpath)
     ms.save_current_mesh(outputpath.replace(".glb", ".obj"), save_textures=False)
     # 调用减面函数
-    courent = trimesh.load(outputpath.replace(".glb", ".obj"), force="mesh")
+    courent = trimesh.load_mesh(outputpath.replace(".glb", ".obj"))
     face_num = courent.faces.shape[0]
 
     if face_num > target_count:
-        courent = courent.simplify_quadric_decimation(target_count)
+        courent = courent.simplify_quadric_decimation(face_count=target_count)
     courent.export(outputpath)
